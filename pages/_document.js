50da78e2b926352712f25daa7e6521fb234785dd
Marcus Faria <mfaria150.github@gmail.com>
2023-01-06 08:05:00 -0300

    feat(head): specified default metadata for Head element
    Seted favicon.ico and base default metadata for document.

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang='pt-BR'>
-      <Head />
+      <Head>
+        <meta charset='UTF-8' />
+        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta name="author" content="Marcus Faria" />
+        <link rel='icon' href='/img/favicon.ico' />
+      </Head>
       <body>
         <Main />
         <NextScript />

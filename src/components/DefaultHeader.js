a1a65b03402316eef4f653ffe925a87e2c2f4663
Marcus Faria <mfaria150.github@gmail.com>
2023-01-11 12:55:33 -0300

    feat(header): created default header component for app

---
+++ src/components/DefaultHeader.js
@@ -0,0 +1,9 @@
+export default function Header(props) {
+  return (
+    <header {...props}>
+      <div data-header className="content__logo">
+        <span> GEN R.O.U.N.D </span>
+      </div>
+    </header>
+  );
+}

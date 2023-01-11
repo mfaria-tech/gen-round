27a97b74fddf2a71bb73af61989318b69f1d64c8
Marcus Faria <mfaria150.github@gmail.com>
2023-01-11 12:56:37 -0300

    feat(sidebar): created sidebar component

---
+++ src/components/SideBar.js
@@ -0,0 +1,17 @@
+export default function SideBar(props) {
+  return (
+    <div data-sidebar {...props}>
+      <ul>
+        <li>
+          <span>icon 1</span>
+        </li>
+        <li>
+          <span>icon 2</span>
+        </li>
+        <li>
+          <span>icon 3</span>
+        </li>
+      </ul>
+    </div>
+  );
+}

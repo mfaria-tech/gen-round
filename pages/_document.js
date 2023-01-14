192e9bbcb95ea6c391b76d582b7bfadca81da1e8
Marcus Faria <mfaria150.github@gmail.com>
2023-01-14 10:19:51 -0300

    perf(head): move metadata to the global app component

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import { Html, Head, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 import Header from '../src/components/DefaultHeader'
 import SideBar from '../src/components/SideBar'
 import WorkSpace from '../src/components/WorkSpace'
@@ -6,17 +6,11 @@ import WorkSpace from '../src/components/WorkSpace'
 export default function Document() {
   return (
     <Html lang='pt-BR'>
-      <Head>
-        <meta charset='UTF-8' />
-        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <meta name="author" content="Marcus Faria" />
-        <link rel='icon' href='/img/favicon.ico' />
-      </Head>
+      <Head />
       <body>
         <Header />
         <SideBar />
-        <WorkSpace />
+        <Main />
         <NextScript />
       </body>
     </Html>

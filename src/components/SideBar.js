232ae71b5dbee5999bcb97ab2fb75d4f91fb9abc
Marcus Faria <mfaria150.github@gmail.com>
2023-01-14 10:22:51 -0300

    feat(sidebar): add icons in the sidebar
    Added icons to fontawesome dependency in the sidebar component

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,17 +1,30 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as iconSet from '@fortawesome/free-solid-svg-icons';
+
 export default function SideBar(props) {
   return (
     <div data-sidebar {...props}>
       <ul>
         <li>
-          <span>icon 1</span>
+          <Icon icon="ChartLine" />
         </li>
         <li>
-          <span>icon 2</span>
+          <Icon icon="FeatherPointed" />
         </li>
         <li>
-          <span>icon 3</span>
+          <Icon icon="Bookmark" />
         </li>
       </ul>
     </div>
   );
+}
+
+function Icon({ icon, ...props }) {
+  return (
+    <FontAwesomeIcon 
+      icon={iconSet[`fa${icon}`]}
+      {...props}
+    />
+  )
 }

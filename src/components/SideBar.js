0da6823cbf7e80074737e17c024b95397206f81d
Marcus Faria <mfaria150.github@gmail.com>
2023-01-17 08:11:28 -0300

    feat(sidebar): update icons
    Remove fontawesome references, and insert static SVG icons

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,31 +1,49 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import * as iconSet from '@fortawesome/free-solid-svg-icons';
+import Image from 'next/image';
+import iconEditNote from '../../public/img/icons/edit_note.svg';
+import iconQueryStats from '../../public/img/icons/query_stats.svg';
+import iconTune from '../../public/img/icons/tune.svg';
 
 export default function SideBar(props) {
   return (
     <div data-sidebar {...props}>
       <ul>
         <li>
-          <Icon icon="ChartLine" title="dashboard" />
+          <Icon icon={iconQueryStats} alt="dashboard" title="dashboard" />
         </li>
         <li>
-          <Icon icon="FeatherPointed" title="write" />
+          <Icon icon={iconEditNote} alt="write" title="write" />
         </li>
         <li>
-          <Icon icon="Bookmark" title="favorite" />
+          <Icon icon={iconTune} alt="settings" title="settings" />
         </li>
       </ul>
     </div>
   );
 }
 
-function Icon({ icon, title, ...props }) {
+// function Icon({ icon, title, ...props }) {
+//   return (
+//     <>
+//       <div data-icon>
+//         <FontAwesomeIcon 
+//           icon={iconSet[`fa${icon}`]}
+//           {...props}
+//         />
+//       </div>
+//       <span>{title}</span>
+//     </>
+//   )
+// }
+
+function Icon({ icon, alt, title, ...props }) {
   return (
     <>
       <div data-icon>
-        <FontAwesomeIcon 
-          icon={iconSet[`fa${icon}`]}
+        <Image 
+          data-icon
+          src={icon}
+          alt={alt}
           {...props}
         />
       </div>

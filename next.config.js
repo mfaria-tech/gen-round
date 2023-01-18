1b573de70eb3366b72d1089da3bec39e32bf4418
Marcus Faria <mfaria150.github@gmail.com>
2023-01-18 07:37:58 -0300

    chore(/public/icons/*): change type of the icons
    Change type images to start to fix on images embedding security

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
+const securityHeaders = [
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+];
+
 module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
+  images: {
+    unoptimized: true,
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 60,
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
 }
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Image from 'next/image';
-import iconEditNote from '../../public/img/icons/edit_note.svg';
-import iconQueryStats from '../../public/img/icons/query_stats.svg';
-import iconTune from '../../public/img/icons/tune.svg';
+import iconEditNote from '../../public/img/icons/edit_note.png';
+import iconMonitoring from '../../public/img/icons/monitoring.png';
+import iconTune from '../../public/img/icons/tune.png';
 
 export default function SideBar(props) {
   return (
     <div data-sidebar {...props}>
       <ul>
         <li>
-          <Icon icon={iconQueryStats} alt="dashboard" title="dashboard" />
+          <Icon icon={iconMonitoring} alt="dashboard" title="dashboard" />
         </li>
         <li>
           <Icon icon={iconEditNote} alt="write" title="write" />
@@ -21,20 +21,6 @@ export default function SideBar(props) {
     </div>
   );
 }
-
-// function Icon({ icon, title, ...props }) {
-//   return (
-//     <>
-//       <div data-icon>
-//         <FontAwesomeIcon 
-//           icon={iconSet[`fa${icon}`]}
-//           {...props}
-//         />
-//       </div>
-//       <span>{title}</span>
-//     </>
-//   )
-// }
 
 function Icon({ icon, alt, title, ...props }) {
   return (
